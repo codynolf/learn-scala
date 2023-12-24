061da658da2db3a7aff831472e230991b7afb515
cody.nolf <codynolf@outlook.com>
2023-12-24 11:12:58 +0000

    WIP on scala functional programming essentials

--- build.sbt
+++ build.sbt
@@ -6,7 +6,12 @@ lazy val playground = (project in file("playground")).settings(
   name := "playground",
   organization := "ch.epfl.scala",
   version := "1.0",
-  libraryDependencies += "org.scodec" %% "scodec-core" % "1.11.9"
+  libraryDependencies += "org.scodec" %% "scodec-core" % "1.11.9",
+  libraryDependencies ++= Seq( 
+    "com.typesafe.akka" %% "akka-http" % "10.5.0",
+    "com.typesafe.akka" %% "akka-actor-typed" % "2.8.0",
+    "com.typesafe.akka" %% "akka-http-spray-json" % "10.5.0"
+  )
 )
 lazy val scalaAtLightspeedBasics= (project in file("scala-at-lightspeed/1-basics"))
 lazy val scalaAtLightspeedOop = (project in file("scala-at-lightspeed/2-oop"))
@@ -15,3 +20,4 @@ lazy val scalaAtLightspeedPatternMatching = (project in file("scala-at-lightspee
 lazy val scalaAtLightspeedAdvanced = (project in file("scala-at-lightspeed/5-advanced"))
 lazy val scalaFpEssentialsBasics= (project in file("scala-functional-programming-essentials/1-basics"))
 lazy val scalaFpEssentialspeedOop = (project in file("scala-functional-programming-essentials/2-oop"))
+
--- scala-functional-programming-essentials/2-oop/src/main/scala/Main.scala
+++ scala-functional-programming-essentials/2-oop/src/main/scala/Main.scala
@@ -1,3 +1,35 @@
 object Main extends App {
-  println("Hello, World!")
+  val person = new Person("Cody", 34)
+  println(person.age)
+  println(person.x)
+  person.greet("Joe")
+  person.greet();
 }
+
+// constructor
+class Person(name: String, val age: Int) {
+  // defines implementation of this class
+  // can have fields(val), methods, expressions
+  // value of code block is ignored, becuase this is the implementation of the class
+  // all expressions will be evaluated, any side effects will execute
+
+  val x = 2
+
+  println(1+3)
+
+  // if name in greet and constuctor are same variable name, they will be same, need to use this.
+  def greet(name: String): Unit = println(s"${this.name} says: Hi, $name")
+
+  // in this greet, this. is implied
+  // overloading function
+  def greet(): Unit = println(s"Hi, I am $name")
+
+
+  // multiple consturctors (overloading constructor)
+  // calls primary constructor with both parameters
+  // these constructors can ONLY call another constructor
+  def this(name: String) = this(name, 0)
+  def this() = this("John Doe")
+}
+
+// class parameters (name: String) are not class fields. Need to add val (val name: String) to make it a class field.
