9e271e4365ac7f11a320f2822a1e6940028de650
cody.nolf <codynolf@outlook.com>
2024-01-19 22:10:21 +0000

    playground code

---
+++ playground/src/main/scala/Collections.scala
@@ -0,0 +1,34 @@
+object Collections extends App {
+    case class SpatialRecord(geometry: Int, changePrevGeom: Boolean)
+
+    // random int 0 or 1
+    val randomInt = scala.util.Random.nextInt(2)
+    
+    // convert 0 or 1 to boolean
+    val randomBool = randomInt match {
+        case 0 => false
+        case 1 => true
+    }
+
+    val spatialRecordsIterator = Iterator.tabulate(5)(i => SpatialRecord(i+scala.util.Random.nextInt(250), scala.util.Random.nextInt(2) match {
+        case 0 => false
+        case 1 => true
+    }))
+
+    val newList = spatialRecordsIterator.foldLeft(List.empty[SpatialRecord])((acc, sr) => {
+        if(acc.isEmpty) {
+            List(sr)
+        } else {
+            val last = acc.last
+            if(last.changePrevGeom) {
+                // change the last element in the list
+                acc.dropRight(1) :+ last.copy(geometry = last.geometry + sr.geometry)
+                acc :+ sr.copy(geometry = last.geometry + sr.geometry)
+            } //else {
+                acc :+ sr
+            //}
+        }
+    })
+
+    val bp = 0
+}
