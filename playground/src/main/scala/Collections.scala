236018f2e6735778326258830cbd9075aea3d15d
cody.nolf <codynolf@outlook.com>
2024-04-01 15:11:34 +0000

    played around with foldLeft

--- playground/src/main/scala/Collections.scala
+++ playground/src/main/scala/Collections.scala
@@ -1,5 +1,5 @@
 object Collections extends App {
-    case class SpatialRecord(geometry: Int, changePrevGeom: Boolean)
+    case class SpatialRecord(geometry: Int, changePrevElementGeom: Int)
 
     // random int 0 or 1
     val randomInt = scala.util.Random.nextInt(2)
@@ -10,24 +10,25 @@ object Collections extends App {
         case 1 => true
     }
 
-    val spatialRecordsIterator = Iterator.tabulate(5)(i => SpatialRecord(i+scala.util.Random.nextInt(250), scala.util.Random.nextInt(2) match {
+    val spatialRecordsIterator = Iterator.tabulate(10)(i => SpatialRecord(i+scala.util.Random.nextInt(250), scala.util.Random.nextInt(2) match {
         case 0 => false
         case 1 => true
     }))
 
-    val newList = spatialRecordsIterator.foldLeft(List.empty[SpatialRecord])((acc, sr) => {
-        if(acc.isEmpty) {
-            List(sr)
-        } else {
-            val last = acc.last
-            if(last.changePrevGeom) {
-                // change the last element in the list
-                acc.dropRight(1) :+ last.copy(geometry = last.geometry + sr.geometry)
-                acc :+ sr.copy(geometry = last.geometry + sr.geometry)
-            } //else {
-                acc :+ sr
-            //}
-        }
+    val newList = spatialRecordsIterator
+        .foldLeft(List.empty[SpatialRecord])((acc, sr) => {
+            if(acc.isEmpty) {
+                List(sr)
+            } else {
+                val last = acc.last
+                if(last.changePrevGeom) {
+                    // change the last element in the list
+                    acc.dropRight(1) :+ last.copy(geometry = last.geometry + sr.geometry)
+                    acc :+ sr.copy(geometry = last.geometry + sr.geometry)
+                } else {
+                    acc :+ sr
+                }
+            }
     })
 
     val bp = 0
