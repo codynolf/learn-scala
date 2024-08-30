04e6d446c1c598d273ad9564a7cfb590f257355a
cody.nolf <codynolf@outlook.com>
2024-08-30 01:52:09 +0000

    buffered producer consumer

--- scala-functional-programming-advanced/src/main/scala/lectures/part3/ThreadCommunication.scala
+++ scala-functional-programming-advanced/src/main/scala/lectures/part3/ThreadCommunication.scala
@@ -1,5 +1,8 @@
 package lectures.part3
 
+import scala.collection.mutable
+import scala.util.Random
+
 object ThreadCommunication extends App{
 
   // the producer-consumer problem
@@ -106,4 +109,56 @@ object ThreadCommunication extends App{
       }
     })
   }
+
+  def prodsConsLargeBuffer(): Unit = {
+    val buffer: mutable.Queue[Int] = new mutable.Queue[Int]
+    val capacity = 3
+
+    val consumer = new Thread(() => {
+      val random = new Random()
+      while (true) {
+        buffer.synchronized {
+          if (buffer.isEmpty) {
+            println("[consumer] buffer empty, waiting...")
+            buffer.wait()
+          }
+
+          // there must be at least one value in the buffer
+          val x = buffer.dequeue()
+          println("[consumer] consumed " + x)
+
+          // hey producer, there's empty space available
+          buffer.notify()
+        }
+        Thread.sleep(random.nextInt(1500))
+      }
+    })
+
+    val producer = new Thread(() => {
+      val random = new Random()
+      var i = 0
+      while (true) {
+        buffer.synchronized {
+          if (buffer.size == capacity) {
+            println("[producer] buffer is full, waiting...")
+            buffer.wait()
+          }
+
+          // there must be at least one empty space in the buffer
+          println("[producer] producing " + i)
+          buffer.enqueue(i)
+
+          // hey consumer, new food for you
+          buffer.notify()
+          i += 1
+        }
+        Thread.sleep(random.nextInt(500))
+      }
+    })
+
+    consumer.start()
+    producer.start()
+  }
+
+  prodsConsLargeBuffer()
 }
